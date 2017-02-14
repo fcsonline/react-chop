90422e5a58df427cd06f819fa25b1dd8628f1a9f
Ferran Basora <fcsonline@gmail.com>
2017-02-14 22:18:10 +0000

    Estimate initial row height + cleaning

--- src/ChopList.js
+++ src/ChopList.js
@@ -1,58 +1,57 @@
 import React, { Component } from 'react';
 
+const DEFAULT_WINDOW_SIZE = 10;
+
 class ChopList extends Component {
+
   constructor(props) {
     super(props);
 
     this.state = {
       offset: 0,
-      scrollTop: 0,
-      internalOffset: 0,
-      windowSize: 30,
-      estimatedHeight: 40,
       overscan: 10,
+      burgerHeight: 0,
     };
   }
 
   componentDidMount() {
-    const { estimatedHeight } = this.state;
+    const childHeights = [...this.refs.innerScrollList.children].map(child => child.offsetHeight);
+    const estimatedHeight = childHeights.reduce((h1, h2) => h1 + h2) / childHeights.length;
 
-    console.log(this.refs.innerScrollList.children.length);
+    this.scrollTop = 0; // Avoid state updates
+    this.estimatedHeight = estimatedHeight; // Avoid state updates
+    this.windowSize = Math.ceil(this.refs.list.offsetHeight / estimatedHeight) + this.state.overscan;
 
-    this.scrollTop = 0;
     this.refs.innerScrollContainer.style.height = `${this.props.rowCount * estimatedHeight}px`;
   }
 
   onScroll(event) {
-    const maxHeight = this.state.estimatedHeight * this.props.rowCount;
-    const overscanHeight = this.state.estimatedHeight * this.state.overscan;
+    const maxHeight = this.estimatedHeight * this.props.rowCount;
+    const overscanHeight = this.estimatedHeight * this.state.overscan;
     const scrollTop = this.refs.list.scrollTop;
 
     if (!scrollTop || (!this.state.offset && scrollTop < 0)) return;
 
-    const realScrollTop = Math.min(Math.max(this.scrollTop + scrollTop * this.state.estimatedHeight, 0), maxHeight);
-    const offset = Math.min(Math.floor(Math.max(scrollTop, 0) / this.state.estimatedHeight), this.props.rowCount - (this.state.windowSize - 200 / this.state.estimatedHeight));
+    const realScrollTop = Math.min(Math.max(this.scrollTop + scrollTop * this.estimatedHeight, 0), maxHeight);
+    const offset = Math.min(Math.floor(Math.max(scrollTop, 0) / this.estimatedHeight), this.props.rowCount - (this.windowSize - 200 / this.estimatedHeight));
     const burgerHeight = !offset ? 0 : Math.max(Math.round(scrollTop - overscanHeight), 0);
 
-    console.log('s', scrollTop, burgerHeight, realScrollTop, offset);
+    // console.log('s', scrollTop, burgerHeight, realScrollTop, offset);
 
-    if (this.refs.burger) {
-      this.refs.burger.style.height = `${burgerHeight}px`;
-    }
+    this.scrollTop = realScrollTop;  // Avoid state updates
 
-    this.scrollTop = realScrollTop;
-
-    this.setState({ offset });
+    this.setState({ offset, burgerHeight });
   }
 
   render() {
-    const { offset, windowSize } = this.state;
+    const { offset, burgerHeight } = this.state;
+    const windowSize = this.windowSize || DEFAULT_WINDOW_SIZE;
 
     return (
       <div ref='list' className="List" onScroll={this.onScroll.bind(this)}>
         <div ref='innerScrollContainer' className="innerScrollContainer">
-        {offset > 0 && (
-          <div ref='burger' className="Burger"/>
+        {burgerHeight > 0 && (
+          <div ref='burger' className="Burger" style={ { height: burgerHeight } }/>
         )}
 
           <div ref='innerScrollList' className="innerScrollList">
